3dcca33a9da7858f20f0e59e80c924ad7c843290
abner <abner.kif1@gmail.com>
2023-12-22 17:05:22 -0300

    resolve error build

--- src/components/TableMui/index.tsx
+++ src/components/TableMui/index.tsx
@@ -67,7 +67,7 @@ export function TableMui() {
     },
   });
 
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const handleChangePage = (_event: unknown, newPage: number) => {
     setPage(newPage);
   };
 
